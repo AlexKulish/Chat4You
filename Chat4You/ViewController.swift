7c7ca82323f8a3bcf16335dffd786711736c97a5
Alexey Kulish <alexkulish1993@gmail.com>
2022-05-31 15:16:30 +0300

    Setup Canvas

--- Chat4You/ViewController.swift
+++ Chat4You/ViewController.swift
@@ -17,3 +17,26 @@ class ViewController: UIViewController {
 
 }
 
+import SwiftUI
+
+struct ViewControllerProvider: PreviewProvider {
+    
+    static var previews: some View {
+        ContainerView().edgesIgnoringSafeArea(.all)
+    }
+    
+    struct ContainerView: UIViewControllerRepresentable {
+        
+        let viewController = ViewController()
+        
+        func makeUIViewController(context: Context) -> some UIViewController {
+            viewController
+        }
+        
+        func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+            
+        }
+        
+    }
+    
+}
