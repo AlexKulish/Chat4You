236d607500edf6b3c13147da3d30de4c86a6242e
Alexey Kulish <alexkulish1993@gmail.com>
2022-06-02 16:24:05 +0300

    Add extension for UISegmentedControl Work in SetupProfileScreen

---
+++ Chat4You/Extensions/SegmentedControl + Extension.swift
@@ -0,0 +1,18 @@
+//
+//  SegmentedControl + Extension.swift
+//  Chat4You
+//
+//  Created by Alex Kulish on 02.06.2022.
+//
+
+import UIKit
+
+extension UISegmentedControl {
+    
+    convenience init(first: String, second: String) {
+        self.init()
+        self.insertSegment(withTitle: first, at: 0, animated: true)
+        self.insertSegment(withTitle: second, at: 1, animated: true)
+        self.selectedSegmentIndex = 0
+    }
+}
--- Chat4You/ViewControllers/SetupProfileViewController.swift
+++ Chat4You/ViewControllers/SetupProfileViewController.swift
@@ -11,6 +11,20 @@ class SetupProfileViewController: UIViewController {
     
     let fullPhotoView = AddPhotoView()
     
+    let welcomeLabel = UILabel(text: "Setup profile", font: .avenir26())
+    let fullNameLabel = UILabel(text: "Full name")
+    let aboutMeLabel = UILabel(text: "About me")
+    let sexLabel = UILabel(text: "Sex")
+    
+    let fullNameTextField = OneLineTextField(font: .avenir20())
+    let aboutMeTextField = OneLineTextField(font: .avenir20())
+    
+    let sexSegmentedControl = UISegmentedControl(first: "Male", second: "Female")
+    
+    let goToChatsButton = UIButton(title: "Go to chats!", titleColor: .white, backgroundColor: .customBlack)
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
@@ -24,13 +38,36 @@ class SetupProfileViewController: UIViewController {
 extension SetupProfileViewController {
     
     private func setupConstraints() {
+        
+        let fullNameStackView = UIStackView(arrangedSubviews: [fullNameLabel, fullNameTextField], axis: .vertical, spacing: 0)
+        let aboutMeStackView = UIStackView(arrangedSubviews: [aboutMeLabel, aboutMeTextField], axis: .vertical, spacing: 0)
+        let sexStackView = UIStackView(arrangedSubviews: [sexLabel, sexSegmentedControl], axis: .vertical, spacing: 0)
+        
+        goToChatsButton.heightAnchor.constraint(equalToConstant: 60).isActive = true
+        let stackView = UIStackView(arrangedSubviews: [fullNameStackView, aboutMeStackView, sexStackView, goToChatsButton], axis: .vertical, spacing: 40)
+        
         fullPhotoView.translatesAutoresizingMaskIntoConstraints = false
+        welcomeLabel.translatesAutoresizingMaskIntoConstraints = false
+        stackView.translatesAutoresizingMaskIntoConstraints = false
         
         view.addSubview(fullPhotoView)
+        view.addSubview(welcomeLabel)
+        view.addSubview(stackView)
         
         NSLayoutConstraint.activate([
-            fullPhotoView.topAnchor.constraint(equalTo: view.topAnchor, constant: 120),
+            welcomeLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 120),
+            welcomeLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            fullPhotoView.topAnchor.constraint(equalTo: welcomeLabel.bottomAnchor, constant: 20),
             fullPhotoView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: fullPhotoView.bottomAnchor, constant: 20),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40)
         ])
         
     }
