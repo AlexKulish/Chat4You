ebacdc156122b4ca15a8b7a8152a050bed9fe7d6
Alexey Kulish <alexkulish1993@gmail.com>
2022-08-03 13:15:50 +0300

    Configure Message Input Bar

--- Chat4You/ViewControllers/Chats/ChatViewController.swift
+++ Chat4You/ViewControllers/Chats/ChatViewController.swift
@@ -28,6 +28,38 @@ class ChatViewController: MessagesViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureMessageInputBar()
+    }
+    
+    private func configureMessageInputBar() {
+        messageInputBar.isTranslucent = true
+        messageInputBar.separatorLine.isHidden = true
+        messageInputBar.backgroundView.backgroundColor = .customWhite
+        messageInputBar.inputTextView.backgroundColor = .white
+        messageInputBar.inputTextView.placeholderTextColor = .customGray
+        messageInputBar.inputTextView.textContainerInset = UIEdgeInsets(top: 14, left: 30, bottom: 14, right: 36)
+        messageInputBar.inputTextView.placeholderLabelInsets = UIEdgeInsets(top: 14, left: 36, bottom: 14, right: 36)
+        messageInputBar.inputTextView.layer.borderColor = #colorLiteral(red: 0.6753501223, green: 0.7336379003, blue: 0.7513690994, alpha: 1)
+        messageInputBar.inputTextView.layer.borderWidth = 0.2
+        messageInputBar.inputTextView.layer.cornerRadius = 18
+        messageInputBar.inputTextView.layer.masksToBounds = true
+        messageInputBar.inputTextView.scrollIndicatorInsets = UIEdgeInsets(top: 14, left: 0, bottom: 14, right: 0)
+        
+        messageInputBar.layer.shadowColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        messageInputBar.layer.shadowRadius = 5
+        messageInputBar.layer.shadowOpacity = 0.3
+        messageInputBar.layer.shadowOffset = CGSize(width: 0, height: 4)
+        
+        configureSendButton()
+    }
+    
+    private func configureSendButton() {
+        messageInputBar.sendButton.setImage(UIImage(named: "Sent"), for: .normal)
+        messageInputBar.sendButton.applyGradients(with: 10)
+        messageInputBar.setRightStackViewWidthConstant(to: 56, animated: false)
+        messageInputBar.sendButton.contentEdgeInsets = UIEdgeInsets(top: 2, left: 2, bottom: 6, right: 30)
+        messageInputBar.sendButton.setSize(CGSize(width: 48, height: 48), animated: false)
+        messageInputBar.middleContentViewPadding.right = -38
     }
     
 }
